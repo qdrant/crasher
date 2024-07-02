d295f809a55140e2f5fcc4579603958c3b242c12
Roman Titov <ffuugoo@users.noreply.github.com>
2024-07-02 13:26:41 +0200

    Cleanup `crash-things.sh` and add `--backup-working-dir` option support

--- crash-things.sh
+++ crash-things.sh
@@ -1,16 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
-QDRANT_DIR=${1:-"./qdrant/"}
-QDRANT_EXEC=${2:-"target/debug/qdrant"}
-CRASH_PROBABILITY=${3:-"0.3"}
-LOG_FILE="crasher_output.log"
-RUN_TIME=${4:-300}
+QDRANT_DIR="${1:-./qdrant/}"
+QDRANT_EXEC="${2:-target/debug/qdrant}"
+CRASH_PROBABILITY="${3:-0.3}"
+RUN_TIME="${4:-300}"
+QDRANT_BACKUP_DIR="${5:-}"
 
-CRASHER_CMD="cargo run -r -- --working-dir $QDRANT_DIR --exec-path $QDRANT_EXEC --crash-probability $CRASH_PROBABILITY --missing-payload-check"
-echo "$CRASHER_CMD"
-$CRASHER_CMD > $LOG_FILE 2>&1 &
+LOG_FILE="crasher_output.log"
+
+CRASHER_CMD=(
+    cargo run --release
+    --
+    --working-dir "$QDRANT_DIR"
+    ${QDRANT_BACKUP_DIR:+--backup-working-dir} "${QDRANT_BACKUP_DIR-}"
+    --exec-path "$QDRANT_EXEC"
+    --crash-probability "$CRASH_PROBABILITY"
+    --missing-payload-check
+)
+
+echo "${CRASHER_CMD[*]}"
+"${CRASHER_CMD[@]}" &>"$LOG_FILE" &
 pid=$!
 
 echo "The PID is $pid"
@@ -19,17 +30,17 @@ function cleanup() {
     kill -9 $pid 2>/dev/null
 }
 
-trap cleanup EXIT
+trap cleanup EXIT INT ERR
 
-if ps -p $pid > /dev/null; then
-    echo "The process is running. Wait for $RUN_TIME seconds."
-    sleep $RUN_TIME
+if ps -p $pid >/dev/null
+then
+    echo "The process is running. Waiting for $RUN_TIME seconds..."
+    sleep "$RUN_TIME"
 
-    if ps -p $pid > /dev/null; then
+    if ps -p $pid >/dev/null
+    then
         echo "The process is still running. Stopping the process..."
-
         kill $pid
-
         echo "OK"
     else
         echo "The process has unexpectedly terminated on its own. Check the logs."
